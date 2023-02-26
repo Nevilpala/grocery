295d518d6a6e3f398b97f4736e3764f5530fad9d
NEVIL PALA <nevilpala306@gmail.com>
2023-02-26 09:42:18 +0530

    api

--- api/nectar/lib/main.dart
+++ api/nectar/lib/main.dart
@@ -17,9 +17,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      // home: const FlashScreen(),
+      home: const FlashScreen(),
       // home:  Demo(title: 'title'),
-      home: const HomePage(),
+      // home: const HomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
